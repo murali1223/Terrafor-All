f3653f8de3561d0a398535b117929ee24f48b183
murali1223 <37334158+murali1223@users.noreply.github.com>
2020-03-14 20:04:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,3 +8,15 @@ resource "aws_instance" "myec2" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
 }
+
+### Provider = its is used to establish the which infrastruture we want 
+### resources = its used to lauch the particular services in the provider 
+
+
+### for download providers we are using ---< terraform init
+### for checking for which resources we are installing ---> terraform plan 
+### for launching terrform we are using ---> terraform apply
+### for destroying infra we are using ---> terraform destroy 
+
+### Destroying particular resources 
+    ---> teraform destroy -target aws_instance.myec2    (terraform destroy -target (resource name.m=our name))
