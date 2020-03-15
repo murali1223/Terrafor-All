1ce200fc3ca0eb6dce28acfce768531f98b7fedb
murali1223 <37334158+murali1223@users.noreply.github.com>
2020-03-15 22:37:58 +0530

    Create provisioner.tf

---
+++ provisioner.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+   key_name = "terraform"
+   
+ provisoner "remote-exec" {
+   inline = [
+       "sudo amazon-linux-extras install -y nginx1.12",
+       "sudo systemctl start nginx"
+       ]
+   connecton {
+     type        = "ssh"
+     host        = "self.public_ip"
+     user        = "ec2-user"
+     private_key = "${file("./terraform.pem")}"
+ 
+     }
+   }
+}   
