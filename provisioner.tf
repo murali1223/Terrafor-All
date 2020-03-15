80edc2ae25f6d6f3100bb49705aada1ff39ed687
murali1223 <37334158+murali1223@users.noreply.github.com>
2020-03-15 23:12:23 +0530

    Update provisioner.tf

--- provisioner.tf
+++ provisioner.tf
@@ -4,11 +4,11 @@ resource "aws_instance" "myec2" {
    key_name = "terraform"
    
  provisoner "remote-exec" {
-   inline = [
+   inline = [            ### inline command is used to execute multiple comands inside Ec2
        "sudo amazon-linux-extras install -y nginx1.12",
        "sudo systemctl start nginx"
        ]
-   connecton {
+   connecton {       ### its a provider connection (connection block)
      type        = "ssh"
      host        = "self.public_ip"
      user        = "ec2-user"
@@ -20,3 +20,25 @@ resource "aws_instance" "myec2" {
 
 ### Provisioner = By using this we have to not aonly created instance , 
                   we have to install nginx server and run automatically by using provisioner 
+
+
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+   key_name = "kplabs-terraform"
+
+   provisioner "remote-exec" {
+     inline = [
+       "sudo amazon-linux-extras install -y nginx1.12",
+       "sudo systemctl start nginx"
+     ]
+
+   connection {
+     type = "ssh"
+     user = "ec2-user"
+     private_key = file("./kplabs-terraform.pem")
+     host = self.public_ip
+   }
+   }
+}
